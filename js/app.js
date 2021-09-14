56a57b909f68daca41c208455356fe4ffd80a421
nganlam0403 <nganlamforwork@gmail.com>
2021-09-14 22:06:56 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const navSlide= function(){
+    const burger= document.querySelector('#burger');
+    const nav = document.querySelector('.nav-links');
+    burger.addEventListener('click',function(){
+        nav.classList.toggle('nav-active');
+    });
+}
+navSlide();
